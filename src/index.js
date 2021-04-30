854b501c99dc54e1aefcd19b92a7d99184b8a238
sowmya <sayanasowmya12@gmail.com>
2021-04-30 19:59:36 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MovieList from './components/movieList.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<MovieList />,
   document.getElementById('root')
 );
 
